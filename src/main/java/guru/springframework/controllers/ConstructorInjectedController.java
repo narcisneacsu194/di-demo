30d9d217dc46abf71356854e9d53296bf899a987
Narcis Neacsu <narcis.neacsu@doc-process.com>
2018-05-27 13:09:21 +0300

    Added Qualifiers to specify what service implementation should be injected.

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,16 +1,14 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
-    @Autowired
-    public ConstructorInjectedController(GreetingService greetingService){
-        this.greetingService = greetingService;
+    public ConstructorInjectedController(GreetingService constructorGreetingService){
+        this.greetingService = constructorGreetingService;
     }
 
     public String sayHello(){
