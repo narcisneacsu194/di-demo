ade5953a2ea1362a83272b1c31b7a6a110eaeacd
Narcis Neacsu <narcis.neacsu@doc-process.com>
2018-05-27 13:38:14 +0300

    Primary Beans.

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -4,6 +4,10 @@ import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.services.ConstructorGreetingService;
+import guru.springframework.services.GreetingServiceImpl;
+import guru.springframework.services.PrimaryGreetingService;
+import guru.springframework.services.SetterGreetingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +20,32 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController)ctx.getBean("myController");
 
-		controller.hello();
+//		controller.hello();
+//
+//		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+//		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 
+//		ConstructorGreetingService constructorGreetingService =
+//				ctx.getBean(ConstructorGreetingService.class);
+//		System.out.println(constructorGreetingService.sayGreeting());
+//
+//		GreetingServiceImpl greetingServiceImpl =
+//				ctx.getBean(GreetingServiceImpl.class);
+//		System.out.println(greetingServiceImpl.sayGreeting());
+//
+//		PrimaryGreetingService primaryGreetingService =
+//				ctx.getBean(PrimaryGreetingService.class);
+//		System.out.println(primaryGreetingService.sayGreeting());
+//
+//		SetterGreetingService setterGreetingService =
+//				ctx.getBean(SetterGreetingService.class);
+//		System.out.println(setterGreetingService.sayGreeting());
+
+		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
 	}
 }
--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -4,12 +4,14 @@ package guru.springframework.controllers;
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
